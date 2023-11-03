d0066ec1c5d5b9d3891581ff7e571198b332c2bb
cnemri <chouaib.nemri@gmail.com>
2023-11-03 19:01:26 +0100

    Added error, loading, middleware and fixed rendering issue

--- app/actions/getCurrentUser.ts
+++ app/actions/getCurrentUser.ts
@@ -7,30 +7,30 @@ export async function getSession() {
 }
 
 export default async function getCurrentUser() {
-  return await getSession()
-    .then(async (session) => {
-      if (session) {
-        if (!session?.user?.email) {
-          return null;
-        }
-        const currentUser = await prisma.user.findUnique({
-          where: {
-            email: session.user.email,
-          },
-        });
-        if (!currentUser) {
-          return null;
-        }
-        return {
-          ...currentUser,
-          createdAt: currentUser.createdAt.toISOString(),
-          updatedAt: currentUser.updatedAt.toISOString(),
-          emailVerified: currentUser.emailVerified?.toISOString() || null,
-        };
-      }
-    })
-    .catch((error) => {
-      console.log(error);
+  try {
+    const session = await getSession();
+
+    if (!session?.user?.email) {
       return null;
+    }
+
+    const currentUser = await prisma.user.findUnique({
+      where: {
+        email: session.user.email as string,
+      },
     });
+
+    if (!currentUser) {
+      return null;
+    }
+
+    return {
+      ...currentUser,
+      createdAt: currentUser.createdAt.toISOString(),
+      updatedAt: currentUser.updatedAt.toISOString(),
+      emailVerified: currentUser.emailVerified?.toISOString() || null,
+    };
+  } catch (error: any) {
+    return null;
+  }
 }
